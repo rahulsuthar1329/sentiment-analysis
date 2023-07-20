77877509580f09420855097568baaabb65170f36
Rahul <rahulsuthar1329@gmail.com>
2023-07-20 18:01:35 +0530

    Bootstrap Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="d-flex justify-content-center align-items-center h-100">
+      <h3 className="bg-success text-white w-100 text-center py-3">
+        Welcome to GenieCart!
+      </h3>
+    </div>
+  );
 }
 
 export default App;
