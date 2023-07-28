e38b15f554b94688f3976924c81d4daff6febb80
Rahul <rahulsuthar1329@gmail.com>
2023-07-28 16:12:06 +0530

    react-svg import removed

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import ReactSVG from "react-svg";
-import mylogo from "./img/Vector.svg";
+import vector from "./img/Vector.svg";
 import styles from "./NotFound.module.css";
 
 const NotFound = () => {
@@ -9,7 +8,7 @@ const NotFound = () => {
   return (
     <div className="h-100 w-100 d-flex justify-content-center align-items-center flex-column">
       <div className="col-10 col-lg-6 col-md-8 col-sm-10 text-center d-flex justify-content-center itc pb-4">
-        <img src={mylogo} alt="404 vector" className="w-100 " />
+        <img src={vector} alt="404 vector" className="w-100 " />
       </div>
       <button onClick={() => navigate(-1)} className={`${styles.goBack}`}>
         Go Back
