5020c8dc5e6210336eaa5528f43dc2c28a036684
Priyanshu Suhalka <priyanshusuhalka@Priyanshus-MacBook-Air.local>
2023-07-31 11:09:47 +0530

    navbar_v1

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import styles from "./LandingPage.module.css";
-import logo from "./images/GenieCart_Teal.png";
 import bag from "./images/vecteezy_illustration-isometric-concept-safety-of-online-shopping-in_5638074.jpg";
+import Navbar from "../Navbar/Navbar";
 
 const LandingPage = () => {
     return (
         <>
             <div className="p-5">
-                <nav className="navbar navbar-light bg-light justify-content">
-                    <img src={logo} width="187" height="50" class="d-inline-block align-top" alt="GenieCart" />
-                    <form className="form-inline">
-                        <button className="btn btn-success mx-3 my-sm-0 " type="submit">Login</button>
-                        <button className="btn btn-outline-success my-sm-0" type="submit">Register</button>
-                    </form>
-                </nav>
+                <Navbar/>
                 <div className={styles.container}>
                     <div className={styles.spacetop}>
                         <h1>
